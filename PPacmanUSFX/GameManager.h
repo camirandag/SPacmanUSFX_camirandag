b2e90a3872a2a256f74f990ea38c418901fd6256
ca mirandag <camirandag11@gmail.com>
2021-07-03 03:34:02 +0200

    factory implementado

--- PPacmanUSFX/GameManager.h
+++ PPacmanUSFX/GameManager.h
@@ -4,10 +4,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <vector>
-
 #include <SDL.h>
 #include <SDL_image.h>
-
 #include "Pacman.h"
 #include "Fantasma.h"
 #include "Fruta.h"
@@ -18,10 +16,7 @@
 #include "TextureManager.h"
 #include "FactoryPacmanClasico.h"
 #include "FactoryPacmanGalactico.h"
-
 using namespace std;
-
-
 //Screen dimension constants
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
